63f5d2f4238428c88ba79565069785efdfcad88c
Ryotaro Shimura <shymul45.k.econ@gamil.com>
2021-05-03 00:00:11 +0900

    complete developing a feature of sending emails

--- src/send_mail.py
+++ src/send_mail.py
@@ -9,34 +9,34 @@ load_dotenv()
 sendgrid_api_key = os.environ.get('API_KEY')
 from_address = os.environ.get('FROM')
 to_list = os.environ.get('TOS').split(',')
- 
-# create a new massage
-message = Mail()
- 
-# setting a sender
-message.from_email = From(from_address)
 
-# setting recivers
-for i in range(len(to_list)):
-    message.to = To(to_list[i])
-    print(to_list[i])
- 
-# decide a subject line
-subject_line = 'The job has completed!'
-message.subject = Subject(subject_line)
- 
-# put a mail body
-mail_body = 'The job worked without problems.\n\nVideo files have been successfully uploaded to S3.' # The job doesn't seem to have worked well.\n\nThere should be something wrong with it.
-message.content = Content(MimeType.text, mail_body)
+def send_email(job_result, mail_body): 
+    # create a new massage
+    message = Mail()
+    
+    # setting a sender
+    message.from_email = From(from_address)
 
-     
-# adding the category information
-message.category = Category('Category1')
-     
-# designate a custome header
-message.header = Header('X-Sent-Using', 'SendGrid-API')
-     
-# send a mail
-sendgrid_client = SendGridAPIClient(sendgrid_api_key)
-sendgrid_client.send(message = message)
+    # setting recivers
+    for i in range(len(to_list)):
+        message.to = To(to_list[i])
+        print(to_list[i])
+    
+    # decide a subject line
+    subject_line = f'The job has {job_result}'# completed!
+    message.subject = Subject(subject_line)
+    
+    # put a mail body
+     # 
+    message.content = Content(MimeType.text, f'{mail_body}')
+    
+    # adding the category information
+    message.category = Category('Category1')
+        
+    # designate a custome header
+    message.header = Header('X-Sent-Using', 'SendGrid-API')
+        
+    # send a mail
+    sendgrid_client = SendGridAPIClient(sendgrid_api_key)
+    sendgrid_client.send(message = message)
 
