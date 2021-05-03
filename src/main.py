6002e30644a02f6f74fd82ad3af16100a4621f0c
Ryotaro Shimura <shymul45.k.econ@gamil.com>
2021-05-04 00:33:12 +0900

    complete developing a feature of getting video files from One Drive

--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
+import os
+import sys
+import glob
+import shutil
+import time
 from send_mail import *
 
+# set variables
+one_drive_path = os.environ["one_drive_path"]
+
+def get_videos_from_one_drive(video_files_path):
+    # if there is not a temp folder, create a temp folder
+    if not (os.path.exists('./tmp')):
+        os.makedirs('./tmp')   
+    # get files whose extensions match with '.MOV' from One Drive
+    video_files = glob.glob(video_files_path)
+    print(video_files)
+    # copy(move) video files from One Drive to ./tmp
+    for video_file in video_files:
+        shutil.move(video_file,'./tmp')
+    
 def main():
-    ### process to upload videos to S3 ###
+    start = time.time()
+    ### download videos from One Drive and store in ./tmp
+    get_videos_from_one_drive(one_drive_path)
+    ### upload videos to S3 ###
 
     ### send an email to notify the result of the job ###
     success = False
@@ -11,9 +33,11 @@ def main():
     else:
         job_result = 'failed'
         mail_body = 'The job does not seem to have worked well.\n\nThere should be something wrong with it.'
-    send_email(job_result, mail_body)
+    # send_email(job_result, mail_body)
+    ### delete ./tmp
 
-
+    elapsed_time = time.time() - start
+    print(elapsed_time)
 
 if __name__ == '__main__':
     main()
